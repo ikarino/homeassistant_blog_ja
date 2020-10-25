b6cf2d3865890d56925fabacc99de5da3c661a56
ikarino <ggrks218@gmail.com>
2020-10-25 10:33:14 +0900

    modification

--- content/RakutenAffiliate.js
+++ content/RakutenAffiliate.js
@@ -18,22 +18,39 @@ const styleImg = {
   objectFit: "contain",
 };
 
+const styleRecommend = {
+  color: "black",
+  backgroundColor: "white",
+  fontSize: "8pt",
+  width: "100%",
+  maxWidth: "600px",
+};
+
 const styleDescription = {
   width: "50%",
 };
-
+//          style="float:left;max-height:27px;width:auto;margin-top:5px"
 function RakutenAffiliate({ url, imgurl, title, value, value_date }) {
   return (
-    <a href={url} style={styleAnchor}>
-      <img src={imgurl} alt={title} style={styleImg} />
-      <div style={styleDescription}>
-        {title.slice(0, 20)}...
-        <br />
-        <b>{value}</b>
-        <br />
-        {value_date}
+    <>
+      <a href={url} style={styleAnchor}>
+        <img src={imgurl} alt={title} style={styleImg} />
+        <div style={styleDescription}>
+          <img src="https://static.affiliate.rakuten.co.jp/makelink/rl.svg" />
+          <br />
+          {title.slice(0, 20)}...
+          <br />
+          <b>{value}</b>
+          <br />
+          {value_date}
+        </div>
+      </a>
+      <div style={styleRecommend}>
+        楽天での購入をオススメする理由については、
+        <a href="/楽天経済圏について">楽天経済圏に関する記事</a>
+        を参考にしてください。
       </div>
-    </a>
+    </>
   );
 }
 
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,39 +1,39 @@
 /** @jsx jsx */
-import { jsx, Heading } from "theme-ui"
-import { MDXRenderer } from "gatsby-plugin-mdx"
-import React from "react"
-import Layout from "./layout"
-import ItemTags from "./item-tags"
-import SEO from "./seo"
+import { jsx, Heading } from "theme-ui";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import React from "react";
+import Layout from "./layout";
+import ItemTags from "./item-tags";
+import SEO from "./seo";
 
 type PostProps = {
   data: {
     post: {
-      slug: string
-      title: string
-      date: string
+      slug: string;
+      title: string;
+      date: string;
       tags?: {
-        name: string
-        slug: string
-      }[]
-      description?: string
-      canonicalUrl?: string
-      body: string
-      excerpt: string
-      timeToRead?: number
+        name: string;
+        slug: string;
+      }[];
+      description?: string;
+      canonicalUrl?: string;
+      body: string;
+      excerpt: string;
+      timeToRead?: number;
       banner?: {
         childImageSharp: {
           resize: {
-            src: string
-          }
-        }
-      }
-    }
-  }
-}
+            src: string;
+          };
+        };
+      };
+    };
+  };
+};
 
-const px = [`32px`, `16px`, `8px`, `4px`]
-const shadow = px.map((v) => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`)
+const px = [`32px`, `16px`, `8px`, `4px`];
+const shadow = px.map((v) => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`);
 
 const Post = ({ data: { post } }: PostProps) => (
   <Layout>
@@ -45,7 +45,14 @@ const Post = ({ data: { post } }: PostProps) => (
       canonicalUrl={post.canonicalUrl}
     />
     <Heading variant="styles.h2">{post.title}</Heading>
-    <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+    <p
+      sx={{
+        color: `secondary`,
+        mt: 3,
+        a: { color: `secondary` },
+        fontSize: [1, 1, 2],
+      }}
+    >
       <time>{post.date}</time>
       {post.tags && (
         <React.Fragment>
@@ -53,19 +60,20 @@ const Post = ({ data: { post } }: PostProps) => (
           <ItemTags tags={post.tags} />
         </React.Fragment>
       )}
-      {post.timeToRead && ` — `}
-      {post.timeToRead && <span>{post.timeToRead} min read</span>}
     </p>
     <section
       sx={{
         my: 5,
-        ".gatsby-resp-image-wrapper": { my: [4, 4, 5], boxShadow: shadow.join(`, `) },
+        ".gatsby-resp-image-wrapper": {
+          my: [4, 4, 5],
+          boxShadow: shadow.join(`, `),
+        },
         variant: `layout.content`,
       }}
     >
       <MDXRenderer>{post.body}</MDXRenderer>
     </section>
   </Layout>
-)
+);
 
-export default Post
+export default Post;
